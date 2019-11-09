7b3d6df862eb6006f43cf7418deb612bb1f9e781
StephensGit <stephenmoran@live.ie>
2019-11-09 00:35:39 +0000

    Updates

---
+++ precache-manifest.de2ed38259bc68fea105ac91cea2c0f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7925691e713f4b4dc4e11a145a98c470",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "96ba407a452a385d06aa",
+    "url": "/assignment/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ede1692e96e48cf84c83",
+    "url": "/assignment/static/css/main.e262d6c8.chunk.css"
+  },
+  {
+    "revision": "96ba407a452a385d06aa",
+    "url": "/assignment/static/js/2.dc6e9f65.chunk.js"
+  },
+  {
+    "revision": "ede1692e96e48cf84c83",
+    "url": "/assignment/static/js/main.8bda7711.chunk.js"
+  },
+  {
+    "revision": "22bb6fdfc39b2c4c2d65",
+    "url": "/assignment/static/js/runtime-main.229548a3.js"
+  }
+]);
